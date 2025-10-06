59a22d30c4203d5bce3484f2c4256c762d4c2387
Mr_Zet <gesmannikita@gmail.com>
2025-10-07 00:32:29 +0300

    Add Calculator(Task1) and AsyncWork(Task2)

---
+++ Task1/ConsoleCalculator.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Calculator
+    {
+        private static readonly char[] operationsArray = ['+', '-', '*', '/'];
+        public static bool IsCorrectInput(string input, ref int number)
+        {
+            try
+            {
+                number = int.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("Неправильное число. Попробуйте ещё раз!");
+                return false;
+            }
+            return true;
+        }
+        public static bool IsCorrectInput(string input, ref char operation)
+        {
+            try
+            {
+                operation = char.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("Неправильная операция. Попробуйте ещё раз!");
+                return false;
+            }
+            return true;
+        }
+        public static bool IsCorrectInput(string input, ref byte choice)
+        {
+            try
+            {
+                choice = byte.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("Неправильный ввод. Попробуйте ещё раз!");
+                return false;
+            }
+            return true;
+        }
+        static int ReadNumber(string message)
+        {
+            int number = 0;
+            while (true)
+            {
+                Console.Write(message);
+                if(IsCorrectInput(Console.ReadLine() ?? "", ref number)) return number;
+            }
+        }
+        static char ReadOperation()
+        {
+            char operation= ' ';
+            while (true)
+            {
+                Console.Write("1. Сложение - \"+\"\n" +
+                    "2. Вычитение - \"-\"\n" +
+                    "3. Умножение - \"*\"\n" +
+                    "4. Деление - \"/\"\n" +
+                    "Введите операцию: ");
+                if (!IsCorrectInput(Console.ReadLine() ?? "", ref operation)) continue;
+                if (!operationsArray.Contains(operation))
+                {
+                    Console.WriteLine("Неправильный символ операции. Попробуйте ещё раз!");
+                    continue;
+                }
+                return operation;
+            }
+        }
+        static double Calculate(int firstNum, int secondNum, char op)
+        {
+            return op switch
+            {
+                '+' => firstNum + secondNum,
+                '-' => firstNum - secondNum,
+                '*' => firstNum * secondNum,
+                '/' => (double)firstNum / secondNum,
+                _ => throw new InvalidOperationException("Неправильная операция!")
+            };
+        }
+        public static void Main()
+        {
+            int firstNumber, secondNumber;
+            char operation;
+            byte userChoice = 0;
+            double result;
+            while (userChoice != 2)
+            {
+                firstNumber = ReadNumber("Введите первое число: ");
+
+                operation = ReadOperation();
+
+                secondNumber = ReadNumber("Введите второе число: ");
+
+                if(secondNumber == 0 && operation == '/')
+                {
+                    Console.WriteLine("Деление на ноль невозможно!");
+                    continue;
+                }
+
+                result = Calculate(firstNumber, secondNumber, operation);
+                Console.WriteLine("Результат: {0}", result);
+
+                while (true)
+                {
+                    Console.Write("1. Продолжить\n" +
+                        "2. Выход\n" +
+                        "Выбор: ");
+                    if (!IsCorrectInput(Console.ReadLine() ?? "", ref userChoice))
+                        continue;
+
+                    if (userChoice == 1 || userChoice == 2)
+                        break;
+
+                    Console.WriteLine("Неверный выбор! Введите 1 или 2");
+                }
+            }
+        }
+    }
+}
---
+++ Task2/HardWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+class HardWork
+{
+    public static string ProcessData(string dataName)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Запуск обработки {dataName} (синхронно)");
+        Thread.Sleep(3000);
+        return $"[{DateTime.Now:HH:mm:ss.fff}] Обработка {dataName} завершена за 3 секунды";
+    }
+
+    public static async Task<string> ProcessDataAsync(string dataName)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Запуск обработки {dataName} (асинхронно)");
+        await Task.Delay(3000);
+        return $"[{DateTime.Now:HH:mm:ss.fff}] Обработка {dataName} завершена за 3 секунды";
+    }
+
+    public static async Task Main()
+    {
+        string[] files = { "Файл 1", "Файл 2", "Файл 3" };
+
+        Console.WriteLine("Синхронная работа");
+        var syncStart = DateTime.Now;
+
+        foreach (var file in files)
+        {
+            var result = ProcessData(file);
+            Console.WriteLine(result);
+        }
+        Console.WriteLine($"Общее время синхронной обработки: {(DateTime.Now - syncStart).TotalSeconds:F1} сек.\n");
+
+        Console.WriteLine("Асинхронная работа");
+        var asyncStart = DateTime.Now;
+        var tasks = files.Select(ProcessDataAsync).ToArray();
+        var results = await Task.WhenAll(tasks);
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+        Console.WriteLine($"Общее время асинхронной обработки: {(DateTime.Now - asyncStart).TotalSeconds:F1} сек.");
+    }
+}
